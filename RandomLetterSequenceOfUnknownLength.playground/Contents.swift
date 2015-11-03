dbd5386460420e60572f7b88c8613809535bc7f5
Russell Gordon <rgordon@rsgc.on.ca>
2015-11-03 06:33:59 -0500

    Initial commit

---
+++ RandomLetterSequenceOfUnknownLength.playground/Contents.swift
@@ -0,0 +1,61 @@
+import Cocoa
+
+/*:
+
+Your prior goal was to:
+
+* generate a string, 20 characters long, with letters randomly selected
+    * based on probability of each letter occuring in all (or at least most) English writing
+
+However, this is not quite how a Markov Chain works.
+
+A Markov chain makes selections based on an analysis of *only* the input text provided.
+
+So, let's move to the next step.
+
+Consider the text below. Re-execute the playground a few times.
+
+*/
+
+let input = getRandomSequence()
+
+input
+
+/*:
+
+What do you notice about the input text each time you run the playground?
+
+Your next step is to take what you have learned from prior playgrounds, and, write an algorithm in plain English that you think would accomplish this goal:
+
+* determine the probability with which a given letter occurs in the input text
+* build an output string that is 30 characters long
+    * letters occur in the output string based on the proabability with which they occurred in the input string
+
+*/
+// Write your algorithm here
+
+/*:
+
+Here is the *first part* of the algorithm we built together in class:
+
+![algorithm-part1](algorithm-part1.png "part1")
+
+Here is the implementation:
+
+*/
+// Implement part one of the algorithm below
+
+/*:
+
+Here is the second part of the algorithm we built today:
+
+![algorithm-part2](algorithm-part2.png "part2")
+
+Here is the implementation:
+
+
+*/
+
+// Implement part one of the algorithm below
+
+
